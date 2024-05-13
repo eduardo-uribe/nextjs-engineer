3d0eb6c1e33ec8c942b22d71643bdb29eb261a6e
javascripty <ed2711uribe@gmail.com>
2024-05-13 14:21:08 -0700

    Add new job post.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,17 +15,17 @@ export default async function Page() {
   return (
     <>
       <nav className='m-6'>
-        <p className='font-medium'>Next.js Engineer</p>
+        <p className='font-semibold text-neutral-900'>Next.js Engineer</p>
       </nav>
       <main className='m-6'>
-        <h1 className='mx-auto w-1/3 text-xl font-medium mb-12'>
+        <h1 className='mx-auto md:w-1/3 font-medium mb-12 text-neutral-900'>
           Join an engineering team developing their product with Next.js your
           React framework of choice.
         </h1>
-        <ul className='mx-auto w-1/3'>
+        <ul className='mx-auto md:w-1/3'>
           {data.map((job: Job) => (
-            <li key='job.job_url' className='mt-10'>
-              <p className='font-medium'>{job.job_title}</p>
+            <li key={job.job_url} className='mt-6 md:mt-10'>
+              <p className='font-semibold text-neutral-900'>{job.job_title}</p>
               <p className='text-zinc-500 text-sm'>{job.job_employer}</p>
               {job.job_salary && (
                 <p className='text-zinc-500 text-sm'>{job.job_salary}</p>
@@ -33,7 +33,10 @@ export default async function Page() {
               {job.job_location && (
                 <p className='text-zinc-500 text-sm'>{job.job_location}</p>
               )}
-              <a className='underline text-sm font-medium' href={job.job_url}>
+              <a
+                className='underline text-sm font-semibold text-neutral-900'
+                href={job.job_url}
+              >
                 Apply
               </a>
             </li>
